100b4ae3802dbff9718c69b04084de26957e48a9
Alan <alau645@aucklanduni.ac.nz>
2015-08-21 00:56:32 +1200

    main menu button text done

--- Assets/Scripts/MainMenuScene.cs
+++ Assets/Scripts/MainMenuScene.cs
@@ -3,9 +3,15 @@ using System.Collections;
 
 public class MainMenuScene : MonoBehaviour {
 
+	private Texture2D numberText;
+	private Texture2D geometryText;
+	private Texture2D statisticsText;
+
 	// Use this for initialization
 	void Start () {
-	
+		numberText = (Texture2D)Resources.Load ("Text/number_and_algebra_text");
+		geometryText = (Texture2D)Resources.Load ("Text/geometry_and_measurement_text");
+		statisticsText = (Texture2D)Resources.Load ("Text/statistics_text");
 	}
 	
 	// Update is called once per frame
@@ -24,19 +30,21 @@ public class MainMenuScene : MonoBehaviour {
 		GUI.Label (new Rect (Screen.width * .0f, Screen.height * .1f, Screen.width * 1.0f, Screen.height * .1f), "SOME TITLE", titleStyle);
 
 		// Number & Algebra button
-		if (GUI.Button (new Rect (Screen.width * .05f, Screen.height * .3f, Screen.width * .25f, Screen.height * .4f), "Number\n&\nAlgebra")) {
+		if (GUI.Button (new Rect (Screen.width * .05f, Screen.height * .3f, Screen.width * .25f, Screen.height * .4f), "")) {
 			// nothing
 		}
+		GUI.DrawTexture (new Rect (Screen.width * .1125f, Screen.height * .425f, Screen.width * .125f, Screen.height * .16f), numberText);
 
 		// Geometry & measurement button
-		if (GUI.Button (new Rect (Screen.width * .375f, Screen.height * .3f, Screen.width * .25f, Screen.height * .4f), "Geometry\n&\nMeasurement")) {
+		if (GUI.Button (new Rect (Screen.width * .375f, Screen.height * .3f, Screen.width * .25f, Screen.height * .4f), geometryText)) {
 			Application.LoadLevel(AppManager.TASK_SELECTION_SCENE);
 		}
 
 		// Statistics button
-		if (GUI.Button (new Rect (Screen.width * .7f, Screen.height * .3f, Screen.width * .25f, Screen.height * .4f), "Statistics")) {
+		if (GUI.Button (new Rect (Screen.width * .7f, Screen.height * .3f, Screen.width * .25f, Screen.height * .4f), "")) {
 			// nothing
 		}
+		GUI.DrawTexture (new Rect (Screen.width * .75f, Screen.height * .475f, Screen.width * .15f, Screen.height * .05f), statisticsText);
 
 		// Teacher's console button
 		if (AppManager.Instance.teacherMode) {
