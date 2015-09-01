4d6e391fe201e238ef3f669baa36e5eee12ddafc
Alan <alau645@aucklanduni.ac.nz>
2015-09-01 12:24:04 +1200

    teacher's login title

--- Assets/Scripts/TeacherLoginScene.cs
+++ Assets/Scripts/TeacherLoginScene.cs
@@ -5,9 +5,11 @@ public class TeacherLoginScene : MonoBehaviour {
 	string password = "";
 	bool keepLoggedIn = false;
 
+	private Texture2D backText;
+
 	// Use this for initialization
 	void Start () {
-	
+		backText = (Texture2D)Resources.Load ("Text/back_text");
 	}
 	
 	// Update is called once per frame
@@ -16,11 +18,6 @@ public class TeacherLoginScene : MonoBehaviour {
 	}
 
 	void OnGUI () {
-		GUIStyle titleStyle = new GUIStyle ("Label");
-		titleStyle.alignment = TextAnchor.UpperCenter;
-		titleStyle.normal.textColor = Color.black;
-		GUI.Label (new Rect (Screen.width * .0f, Screen.height * .1f, Screen.width * 1.0f, Screen.height * .1f), "This is a Teacher's only Area!", titleStyle);
-
 		GUI.Box (new Rect (Screen.width * .25f, Screen.height * .2f, Screen.width * .5f, Screen.height * .6f), "");
 
 		// password
@@ -36,10 +33,10 @@ public class TeacherLoginScene : MonoBehaviour {
 		}
 
 		// keep logged in
-		keepLoggedIn = GUI.Toggle(new Rect(Screen.width * .4f, Screen.height * .65f, Screen.width * .25f, Screen.height * .07f), keepLoggedIn, "  Keep logged in");
+		//keepLoggedIn = GUI.Toggle(new Rect(Screen.width * .4f, Screen.height * .65f, Screen.width * .25f, Screen.height * .07f), keepLoggedIn, "  Keep logged in");
 
 		// back button
-		if (GUI.Button (new Rect (Screen.width * .0f, Screen.height * .9f, Screen.width * .2f, Screen.height * .1f), "Back")) {
+		if (GUI.Button (new Rect (Screen.width * .0f, Screen.height * .9f, Screen.width * .2f, Screen.height * .1f), backText)) {
 			Application.LoadLevel(AppManager.START_SCENE);
 		}
 	}
