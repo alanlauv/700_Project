a01e06022fd1cfb156a86fa813a5b35513fad8c5
Alan <alau645@aucklanduni.ac.nz>
2015-09-02 23:44:54 +1200

    q6 hitn and ID done

--- Assets/Scripts/Y1Q6Scene.cs
+++ Assets/Scripts/Y1Q6Scene.cs
@@ -4,26 +4,9 @@ using System.Collections;
 public class Y1Q6Scene : MonoBehaviour {
 	public const string MEASUREMENT_Y1Q6 = "Measurement/Y1/Q6";
 
-	private bool displayHelpButton = false;
-	private bool displayRedCross = false;
-	private bool displayLadybug = false;
-	private bool displayHint = false;
-	
-	private int numIncorrect = 0;
-	
-	// update data timer
-	private float timer = 0.0f;
-	private float timerMax = 15.0f;
-	
-	private float crossTimer = 0.0f;
-	private float crossTimerMax = 3.0f;
-	
 	//textures
-	private Texture2D redCross;
 	private Texture2D ladybug;
-	//settings & help icon
-	private Texture2D helpIcon;
-	private Texture2D hint;
+
 	// text
 	private Texture2D greenPencilText;
 	private Texture2D yellowPencilText;
@@ -32,9 +15,7 @@ public class Y1Q6Scene : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		redCross = (Texture2D)Resources.Load("red-cross");
 		ladybug = (Texture2D)Resources.Load("pics/Lady-Bug_r");
-		helpIcon = (Texture2D)Resources.Load ("pics/green_hand");
 
 		greenPencilText = (Texture2D)Resources.Load ("Text/green_pencil_text");
 		yellowPencilText = (Texture2D)Resources.Load ("Text/yellow_pencil_text");
@@ -47,73 +28,41 @@ public class Y1Q6Scene : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		timer += Time.deltaTime;
-		if (displayHelpButton == false && timer >= timerMax) {
-			//Debug.Log("timerMax reached!");
-			displayHelpButton = true;
-		}
-		
-		if (displayRedCross) {
-			crossTimer += Time.deltaTime;
-			if (crossTimer >= crossTimerMax) {
-				displayRedCross = false;
-				crossTimer = 0.0f;
-			}
-		}
-		
-		if (displayLadybug == false && numIncorrect >= 2) {
-			displayLadybug = true;
-			displayHelpButton = true;
-		}
+
 	}
 	
 	void OnGUI () {
 		if (!SettingsDialog.displaySettings) {
-			// help dialog button (20sec wait) and display astronauts
-			if (displayHelpButton) {
-				if (GUI.Button (new Rect (Screen.width * .89f, Screen.height * .0f, Screen.width * .05f, Screen.width * .05f), helpIcon)) {
-					displayLadybug = true;
-					numIncorrect++;
-				}
-			}
-		
 			// answer pool
 			// Yellow
 			if (GUI.Button (new Rect (Screen.width * .15f, Screen.height * .15f, Screen.width * .2f, Screen.height * .1f), greenPencilText)) {
-				displayRedCross = true;
-				numIncorrect++;
+				IncorrectDialog.displayIncorrectDialog = true;
+				StarDialog.numIncorrect++;
 			}
 		
 			// green pencil
 			if (GUI.Button (new Rect (Screen.width * .15f, Screen.height * .35f, Screen.width * .2f, Screen.height * .1f), bluePencilText)) {
-				displayRedCross = true;
-				numIncorrect++;
+				IncorrectDialog.displayIncorrectDialog = true;
+				StarDialog.numIncorrect++;
 			}
 
 			// blue pencil
 			if (GUI.Button (new Rect (Screen.width * .15f, Screen.height * .55f, Screen.width * .2f, Screen.height * .1f), pinkPencilText)) {
-				AppManager.Instance.storeNumIncorrect (numIncorrect);
+				StarDialog.displayStars = true;
 			}
 
 			//pink pencil
 			if (GUI.Button (new Rect (Screen.width * .15f, Screen.height * .75f, Screen.width * .2f, Screen.height * .1f), yellowPencilText)) {
-				displayRedCross = true;
-				numIncorrect++;
+				IncorrectDialog.displayIncorrectDialog = true;
+				StarDialog.numIncorrect++;
 			}
 		
 			drawLadyBug ();
-			drawRedCross ();
-		}
-	}
-	
-	private void drawRedCross () {
-		if (displayRedCross) {
-			GUI.DrawTexture(new Rect(Screen.width * .25f, Screen.height * .05f, Screen.width * .5f, Screen.width * .5f), redCross);
 		}
 	}
 
 	private void drawLadyBug () {
-		if (displayLadybug) {
+		if (HintButton.displayHint) {
 			//green
 			//GUI.DrawTexture(new Rect(Screen.width * .67f, Screen.height * .12f, Screen.width * .04f, Screen.height * .06f), ladybug);
 			//GUI.DrawTexture(new Rect(Screen.width * .72f, Screen.height * .12f, Screen.width * .04f, Screen.height * .06f), ladybug);
