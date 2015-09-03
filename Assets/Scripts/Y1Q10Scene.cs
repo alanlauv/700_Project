bda24e9ce10642a25e0b0c5c707c19481922a567
Alan <alau645@aucklanduni.ac.nz>
2015-09-03 15:59:35 +1200

    q10 relocated counter

--- Assets/Scripts/Y1Q10Scene.cs
+++ Assets/Scripts/Y1Q10Scene.cs
@@ -25,7 +25,7 @@ public class Y1Q10Scene : MonoBehaviour {
 	void OnGUI () {
 		if (!SettingsDialog.displaySettings) {
 			// blast off button
-			if (GUI.Button (new Rect (Screen.width * .45f, Screen.height * .85f, Screen.width * .2f, Screen.height * .1f), blastOff)) {
+			if (GUI.Button (new Rect (Screen.width * .26f, Screen.height * .85f, Screen.width * .2f, Screen.height * .1f), blastOff)) {
 				if (Counter.counter == 4) {
 					StarDialog.displayStars = true;
 					AppManager.Instance.addCompletedTask (MEASUREMENT_Y1Q10, StarDialog.numIncorrect, HintButton.hintUsed);
