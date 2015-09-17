d115a6037f7e4754ae39ad759634123e0237404a
Alan <alau645@aucklanduni.ac.nz>
2015-09-17 17:24:39 +1200

    scroll reduced

--- Assets/Scripts/TeacherCompletedTasksScene.cs
+++ Assets/Scripts/TeacherCompletedTasksScene.cs
@@ -16,7 +16,7 @@ public class TeacherCompletedTasksScene : MonoBehaviour {
 	}
 
 	void OnGUI () {
-		scrollPosition = GUI.BeginScrollView(new Rect(10, 0, Screen.width * 0.98f, Screen.height * 1f), scrollPosition, new Rect(0, 0, Screen.width * 0.94f, Screen.height * .1f * 500));
+		scrollPosition = GUI.BeginScrollView(new Rect(0, 0, Screen.width * 0.98f, Screen.height * 1f), scrollPosition, new Rect(0, 0, Screen.width * 0.94f, Screen.height * .1f * 5));
 
 		// font size
 		GUIStyle style = new GUIStyle ();
@@ -26,12 +26,12 @@ public class TeacherCompletedTasksScene : MonoBehaviour {
 		// display completed tasks
 		GUI.Label(new Rect (Screen.width * .05f, Screen.height * .05f, Screen.width * .95f, Screen.height * .95f), AppManager.Instance.loadCompletedTasks (), style);
 
+		GUI.EndScrollView();
+
 		// back button
 		if (GUI.Button (new Rect (Screen.width * .0f, Screen.height * .9f, Screen.width * .1f, Screen.height * .1f), "Back")) {
 			// move to teacher console
 			Application.LoadLevel(AppManager.TEACHER_SCENE);
 		}
-
-		GUI.EndScrollView();
 	}
 }
